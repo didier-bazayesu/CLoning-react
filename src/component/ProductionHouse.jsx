4b2f28dfef7563f8dc19de59f8d3541a117da164
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-03 21:43:38 +0200

    feat : adding videos and image for use

--- src/component/ProductionHouse.jsx
+++ src/component/ProductionHouse.jsx
@@ -1,4 +1,40 @@
 import React from 'react'
+ import { useState } from 'react'
+import logs from '../assets/icons8-disney.png'
+
+
+
+
+let allProduction = [
+
+  {name : 'Marvel studio ',
+    video : '',
+    image : logs
+  },
+    {
+        name: 'Marvel studio ',
+        video: '',
+        image: logs
+    },
+    {
+        name: 'Marvel studio ',
+        video: '',
+        image: logs
+    },
+    {
+        name: 'Marvel studio ',
+        video: '',
+        image: logs
+    },
+    {
+        name: 'Marvel studio ',
+        video: '',
+        image: logs
+    }
+
+
+
+]
 
 function ProductionHouse() {
   return (
