2a780ad3d947324a8ae90fde50b7e355eeed1293
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-03 21:23:55 +0200

    feat: adding feature

--- src/component/Slider.jsx
+++ src/component/Slider.jsx
@@ -1,5 +1,6 @@
-import React, {useEffect,useState} from 'react'
+import React, {useEffect,useRef,useState} from 'react'
 import GlobalApi from '../services/GlobalApi';
+import {  HiChevronLeft, HiChevronRight } from "react-icons/hi2";
 
 
 
@@ -8,6 +9,18 @@ import GlobalApi from '../services/GlobalApi';
      const baseUrl = "https://image.tmdb.org/t/p/original"
      
      let[movieList,setMovieList]= useState([])
+     let elementRef = useRef()
+     let windowWidth = window.innerHeight+500
+
+     let sliderLeft = (element)=>{
+        element.scrollLeft-=windowWidth;
+     }
+     let sliderRight = (element) => {
+         element.scrollLeft += windowWidth;
+     }
+    
+   
+     
 
 
     useEffect(()=>{
@@ -25,18 +38,43 @@ import GlobalApi from '../services/GlobalApi';
         })
     }
      
-  return (
-      <div className='flex overflow-x-auto w-full px-16 py-4  scrollbar-none class'>
+    //  movieList.forEach(movie => {
+    //      console.log(movie.
+    //          original_title);
+    //  })
 
-        {movieList.map((elem,index)=> { 
-           
-           return(
-            <img id={index} src={baseUrl + elem.backdrop_path} alt="" className='min-w-full h-[400px] object-cover
-            object-left-top m-5 rounded-md  '/>
-           )
+  return (
+
+    <div className='flex cursor-pointer '>
+        <HiChevronLeft className='hidden md:block text-white text-[50px]  mt-[170px] ml-4 absolute'
+         onClick={()=>sliderLeft(elementRef.current)}/>
+
+        <div className='flex overflow-x-auto w-full px-16 py-4   
+         scroll-smooth scrollbar-none class'  ref={elementRef} >
+
+            {movieList.map((elem,index)=> { 
+            return(
             
-        })}
+               <>
+               <img  src={baseUrl + elem.backdrop_path} alt="" className='w-auto md:min-w-full  md:h-[400px] object-cover
+               object-left-top m-5 rounded-md  hover:border-6 hover:border-white  hover:transition-all  ease-in'/>
+                    {/* <p>{elem.original_title}</p> */}
+               
+               </>
+
+                
+                
+            )
+                
+            })}
+        </div>
+
+          <HiChevronRight className='hidden md:block text-white text-[50px] 
+           mt-[170px] ml-4 absolute right-0 ' onClick={() => sliderRight(elementRef.current)}/>
+
     </div>
+           
+        
   )
 }
 
