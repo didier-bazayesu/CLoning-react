03d6c3b3a49669d5f6f6c94f73d052008ab9e1a5
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-13 17:00:52 +0200

    doc: ProductionList

---
+++ src/component/Constant/GeneralList.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function GeneralList() {
+
+
+  let store = [
+
+{id: 1 , name : 'Action' },
+{id:2 ,name : 'Adventure'},
+{id:3 , name :'Animation'},
+{id:4 , name : 'Codmedy'}
+
+
+  ]
+  return (
+    <div>GeneralList</div>
+  )
+}
+
+export default GeneralList
