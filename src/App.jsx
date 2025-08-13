e79c5d27380ef65cf0b66ed3ee42266946b99ec3
didier-bazayesu <didierbazayesu@gmail.com>
2025-08-13 16:54:50 +0200

    del: mental.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './component/Header';
 import Slider from './component/Slider';
 import ProductionHouse from './component/ProductionHouse';
-import Mental from './component/mental';
+
 
 
 
